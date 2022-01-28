fecf5ae635aac4e76346712bb0ed65d4fb902379
Ararat01 <ararat12bars@gmail.com>
2022-01-28 19:10:43 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    LogoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
